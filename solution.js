7e7d7563430aa2ca21215480f7c0f5fdc1ac9530
Anilkokkul <anilkokkul1997@gmail.com>
2024-07-19 18:45:10 +0530

    "Updated coinChange function implementation with new dynamic programming approach"

--- solution.js
+++ solution.js
@@ -32,12 +32,15 @@ let amount = 11;
 console.log(coinChange(coins, amount));
 
 function coinChange(coins, amount) {
-  let dp = Array(amount + 1).fill(Infinity);
+  const dp = new Array(amount + 1).fill(amount + 1);
+
   dp[0] = 0;
-  for (let coin of coins) {
-    for (let x = coin; x <= amount; x++) {
-      dp[x] = Math.min(dp[x], dp[x - coin] + 1);
+  for (let i = 0; i <= amount; i++) {
+    for (let coin of coins) {
+      if (i - coin >= 0) {
+        dp[i] = Math.min(dp[i], dp[i - coin] + 1);
+      }
     }
   }
-  return dp[amount] === Infinity ? -1 : dp[amount];
+  return dp[amount] === 0 ? -1 : dp[amount];
 }
